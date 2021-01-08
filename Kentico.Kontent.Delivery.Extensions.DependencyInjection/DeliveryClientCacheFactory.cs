643d115edfa414b51ad824e21cfa81c40025eaaf
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2021-01-08 14:41:57 +0100

    extensions package for a named client

--- Kentico.Kontent.Delivery.Extensions.DependencyInjection/DeliveryClientCacheFactory.cs
+++ Kentico.Kontent.Delivery.Extensions.DependencyInjection/DeliveryClientCacheFactory.cs
@@ -16,7 +16,6 @@ namespace Kentico.Kontent.Delivery.Extensions.DependencyInjection
         private readonly IDeliveryClientFactory _innerDeliveryClientFactory;
         private readonly ConcurrentDictionary<string, IDeliveryClient> _cache = new ConcurrentDictionary<string, IDeliveryClient>();
 
-
         /// <summary>
         /// Initializes a new instance of the <see cref="DeliveryClientCacheFactory"/> class.
         /// </summary>
